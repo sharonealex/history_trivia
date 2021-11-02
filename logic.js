577cf64b3531b094a227a781aaefa196219930f8
Sharon <sharonelizalex@gmail.com>
2021-11-02 13:01:42 +1100

    added functionality for quiz and scores

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 // variables to reference DOM elements
-var questionsEl = document.getElementById("questions");
+var questionsEl = document.querySelector("#questions");
 var timerEl = document.querySelector(".timer");
 var timerSpanEl = document.querySelector(".timeSpan");
 var choicesEl = document.getElementById("choices");
@@ -12,21 +12,72 @@ var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
 
+function endQuiz(){
+  
+    clearInterval(timerId);
+    var endScreen = document.querySelector("#end-screen");
+    endScreen.removeAttribute("class");
+
+    var finalScoreEl = document.querySelector("#final-score");
+    finalScoreEl.textContent = time;
+
+    questionsEl.setAttribute("class", "hide");
+
+}
+
+function questionClick(choice){
+    if (choice !== questions[currentQuestionIndex].answer){
+        time = time - 15;
+       feedbackEl.textContent = "Wrong!"
+    } else {
+        feedbackEl.textContent = "Correct!"
+    }
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function(){
+        feedbackEl.setAttribute("class", "hide")
+    },1000)
+
+    currentQuestionIndex++;
+
+    if(currentQuestionIndex === questions.length){
+        alert("ehy")
+        endQuiz();
+    }else {
+        getQuestion();
+    }
+
+}
+
+function getQuestion(){
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl = document.querySelector("#question-title");
+    titleEl.textContent = "";
+    choicesEl.textContent = "";
+    titleEl.textContent = currentQuestion.title;
+    currentQuestion.choices.forEach(function(el){
+        var choiceButton = document.createElement("button")
+        choiceButton.value = el;
+        choiceButton.textContent = el;
+        choicesEl.appendChild(choiceButton);
+        choiceButton.addEventListener('click', function(){
+            questionClick(this.value)
+        })
+    })
+
+}
+
 function startQuiz() {
     // hide start screen
     var startScreenEl = document.getElementById("start-screen");
     startScreenEl.setAttribute("class", "hide");
   
     // un-hide questions section
-   // questionsEl.removeAttribute("class");
+    questionsEl.removeAttribute("class");
+    questionsEl.setAttribute("class", "border")
   
     // start timer
     timerId = setInterval(clockTick, 1000);
-  
-    // show starting time
-    //timerEl.textContent = time;
-  
-    //getQuestion();
+    getQuestion();
   }
 
   
@@ -38,7 +89,7 @@ function clockTick() {
   
     // check if user ran out of time
     if (time <= 0) {
-      quizEnd();
+     endQuiz();
     }
   }
 
